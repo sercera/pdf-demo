3e3f9c4d6957fc5bafdde65faac5bf9f94a97e13
Uros Ceranic <ceranic.uros96@gmail.com>
2020-06-02 17:51:21 +0200

    FIX

--- src/routes/pdf/index.js
+++ src/routes/pdf/index.js
@@ -3,26 +3,51 @@ const express = require('express');
 const router = express.Router();
 const puppeteer = require('puppeteer');
 
-router.get('/generate', async (req, res) => {
+router.get('/generate-image', async (req, res) => {
   const { url } = req.query;
   let browser = null;
   try {
-    browser = await puppeteer.launch({ headless: true });
+    browser = await puppeteer.launch({ headless: true, args: ['--window-size=1980,1024'] });
     const page = await browser.newPage();
 
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.goto(url, { waitUntil: 'networkidle0' });
 
-    const title = await page.title();
+    const pdfName = (url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)/) || [])[1];
 
+    // await page.waitFor(30000);
+    const image = await page.screenshot({ fullPage: true });
+    res.setHeader('Content-Length', image.length);
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader('Content-Disposition', `inline; filename=${pdfName || 'site'}.png`);
+    return res.send(image);
+  } catch (e) {
+    if (browser) {
+      console.log(e);
+      await browser.close();
+    }
+  }
+});
+
+router.get('/generate-pdf', async (req, res) => {
+  const { url } = req.query;
+  let browser = null;
+  try {
+    browser = await puppeteer.launch({ headless: true, args: ['--window-size=1980,1024'] });
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    const pdfName = (url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)/) || [])[1];
     const pdfData = await page.pdf({
       format: 'A4',
       printBackground: true,
-    });
+      preferCSSPageSize: true,
 
+    });
     await browser.close();
     res.setHeader('Content-Length', pdfData.length);
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `inline; filename=${title}.pdf`);
+    res.setHeader('Content-Disposition', `inline; filename=${pdfName || 'site'}.pdf`);
     return res.send(pdfData);
   } catch (e) {
     if (browser) {
